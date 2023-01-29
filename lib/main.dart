0a4a801aef6fa090144ef679b9eaed934bf44b2e
kayyum0 <sd21if002@gmail.com>
2023-01-29 14:57:34 +0530

    Login Page in flutter

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+import 'package:login/login.dart';
+import 'package:login/register.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+    routes: {
+      'login': (context) =>MyLogin(),
+      'register': (context) =>MyRegister(),
+    },
+  ));
+}
